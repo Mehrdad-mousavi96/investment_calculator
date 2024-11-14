d3a6711a824ff085e7c178e988c5cf6ca4cdabb2
Mehrdad <mehrdad.fmousavi@gmail.com>
2024-11-14 10:56:09 +0330

    add header and userinput components (also css) and defining some inputs for investment

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Main from './investment-calculator/Main'
+
+const App = () => {
+  return (
+    <div>
+      <Main />
+    </div>
+  )
+}
+
+export default App
---
+++ src/investment-calculator/Main.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <UserInput />
+    </div>
+  );
+};
+
+export default App;
