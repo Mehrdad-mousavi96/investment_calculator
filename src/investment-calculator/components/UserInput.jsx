be66cf263a7fc6e27abb9ffc1e9de8df7f9d6437
Mehrdad <mehrdad.fmousavi@gmail.com>
2024-11-14 11:32:44 +0330

    We separated states and function into main component and props passed to them

--- src/investment-calculator/components/UserInput.jsx
+++ src/investment-calculator/components/UserInput.jsx
@@ -1,37 +1,65 @@
 import React, { useState } from "react";
 
 const UserInput = () => {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 1000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
 
-    const [] = useState({
-        initialInvestment: 1000,
-        annualInvestment: 1200,
-        expectedReturn: 6,
-        duration: 10
-    })
+  const handleChagnes = (inputIdentifires, newValue) => {
+    setUserInput((prevUserInput) => {
+      return {
+        ...userInput,
+        [inputIdentifires]: newValue,
+      };
+    });
+  };
 
   return (
     <section id="user-input">
       <div className="input-group">
         <p>
           <label>Initial Investment</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            value={userInput.initialInvestment}
+            onChange={(e) => handleChagnes("initialInvestment", e.target.value)}
+          />
         </p>
 
         <p>
           <label>Annual Investment</label>
-          <input type="number" required/>
+          <input
+            type="number"
+            required
+            value={userInput.annualInvestment}
+            onChange={(e) => handleChagnes("annualInvestment", e.target.value)}
+          />
         </p>
       </div>
-      
+
       <div className="input-group">
         <p>
           <label>Expected Return</label>
-          <input type="number" required/>
+          <input
+            type="number"
+            required
+            value={userInput.expectedReturn}
+            onChange={(e) => handleChagnes("expectedReturn", e.target.value)}
+          />
         </p>
 
         <p>
           <label>Duration</label>
-          <input type="number" required/>
+          <input
+            type="number"
+            required
+            value={userInput.duration}
+            onChange={(e) => handleChagnes("duration", e.target.value)}
+          />
         </p>
       </div>
     </section>
