672147a74d8ea6b400888a910406440040076d86
Mehrdad <mehrdad.fmousavi@gmail.com>
2024-11-17 09:47:54 +0330

    we cam take books name and save it in our state and also update it

--- src/Stephen Grider/book app/App_.jsx
+++ src/Stephen Grider/book app/App_.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import BookCreate from './components/BookCreate'
 
 const App_ = () => {
+    const [books, setBooks] = useState([])
+
+    const handleCreateBook = (title) => {
+        console.log("Need to add book with:", title)
+    }
+     
   return (
     <div>
-        
+        <BookCreate onCreate={handleCreateBook} />
     </div>
   )
 }
--- src/Stephen Grider/book app/components/BookCreate.jsx
+++ src/Stephen Grider/book app/components/BookCreate.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const BookCreate = () => {
+const BookCreate = ({ onCreate }) => {
+  const [title, setTitle] = useState("");
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onCreate(title)
+  }
+
   return (
-    <div>BookCreate</div>
-  )
-}
+    <div onSubmit={handleSubmit}>
+      <form>
+        <label>Title</label>
+        <input value={title} onChange={handleChange} />
+        <button>Create!</button> 
+      </form>
+    </div>
+  );
+};
 
-export default BookCreate
+export default BookCreate;
